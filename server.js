6b21a3a0142697115a1da4d407470aaae1c488ab
Jacob Bleser <jacobbleser@aol.com>
2016-09-19 20:44:39 +0000

    Added header parsing in json format

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 var express = require("express");
 var app = express();
+
+app.get('/', function(req, res){
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+        lang = req.headers["accept-language"].slice(0, req.headers["accept-language"].indexOf(","));
+    var software = req.headers['user-agent'];
+    
+    var os = software.slice(software.indexOf("(")+1, software.indexOf(")"));
+    
+    var json = {
+        "ip-address": ip,
+        "language": lang,
+        "system": os
+    }
+    
+    res.send(json);
+});
+
+
+app.listen(process.env.PORT || 8080);
